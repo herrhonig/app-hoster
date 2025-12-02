0b7680fbffbe360025be4925ac54c16ad0a7f7c5
nicknick <nickback0@yandex.ru>
2025-12-02 21:49:17 +0300

    feat: Added root config files. Added simple react-couter app for testing

---
+++ packages/react-vite-counter/src/app.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex flex-col items-center gap-4 p-6">
+      <h1 className="text-2xl font-bold">React Tailwind Counter</h1>
+
+      <div className="text-lg font-semibold">Count: {count}</div>
+
+      <div className="flex gap-3">
+        <button
+          onClick={() => setCount(count + 1)}
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+        >
+          +
+        </button>
+
+        <button
+          onClick={() => setCount(count - 1)}
+          className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ packages/react-vite-counter/vite.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/apps/react-counter/",
+});
