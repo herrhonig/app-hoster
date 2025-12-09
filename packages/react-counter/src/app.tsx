82948ddab7ebef6eb161282bec4a827f7a109105
nicknick <nickback0@yandex.ru>
2025-12-09 21:42:55 +0300

    feat: changed img styles, changed deploy yml

--- packages/react-counter/src/app.tsx
+++ packages/react-counter/src/app.tsx
@@ -7,39 +7,34 @@ export default function App() {
   const toggleBanner = () => setBanner((prevBool) => !prevBool);
 
   return (
-    <div className="w-full h-full grid grid-cols-2 gap-16">
-      <div className="flex flex-col items-center gap-4 p-6">
-        <h1 className="text-2xl font-bold">React Tailwind Counter</h1>
+    <div className="flex flex-col items-center gap-4 p-6">
+      <h1 className="text-2xl font-bold">React Tailwind Counter</h1>
 
-        <div className="text-lg font-semibold">Count: {count}</div>
+      <div className="text-lg font-semibold">Count: {count}</div>
 
-        <div className="flex gap-3">
-          <button
-            onClick={() => setCount(count + 1)}
-            className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-          >
-            +
-          </button>
-
-          <button
-            onClick={() => setCount(count - 1)}
-            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
-          >
-            -
-          </button>
-        </div>
-        <br />
-        <br />
-        <div
-          role="button"
-          className="w-full bg-amber-300 rounded-2xl cursor-pointer"
+      <div className="flex gap-3">
+        <button
+          onClick={() => setCount(count + 1)}
+          className="bg-blue-600 text-gray-800 px-4 py-2 rounded hover:bg-blue-700 transition"
         >
-          <div
-            onClick={toggleBanner}
-            className="text-center text-amber-800 font-bold text-xl"
-          >
-            New updates!
-          </div>
+          +
+        </button>
+        <button
+          onClick={() => setCount(count - 1)}
+          className="bg-red-600 text-gray-800 px-4 py-2 rounded hover:bg-red-700 transition"
+        >
+          -
+        </button>
+      </div>
+      <div
+        role="button"
+        className="w-full bg-amber-300 rounded-2xl cursor-pointer"
+      >
+        <div
+          onClick={toggleBanner}
+          className="text-center text-amber-800 font-bold text-xl"
+        >
+          New updates!
         </div>
       </div>
 
@@ -48,8 +43,7 @@ export default function App() {
           <img
             src="/assets/word-update.png"
             alt="banner"
-            width={600}
-            height={600}
+            className="w-64 absolute top-2 left-2.5"
           />
         </div>
       )}
